2eea845a113c756b3fde669fc7185171d447baa9
dfortuna <denis.fortuna@gmail.com>
2020-10-27 15:42:49 -0300

    first commit

---
+++ Prova_HelpieTests/Prova_HelpieTests.swift
@@ -0,0 +1,34 @@
+//
+//  Prova_HelpieTests.swift
+//  Prova_HelpieTests
+//
+//  Created by Denis Fortuna on 27/10/20.
+//  Copyright © 2020 Denis Fortuna. All rights reserved.
+//
+
+import XCTest
+@testable import Prova_Helpie
+
+class Prova_HelpieTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,16 @@
+# Prova_Helpie
+
+Prova_Helpie is an iOS application developed using Swift as an attempt to the Helpie mobile code challenge
+
+
+## Libraries
+
+
+## Requiriments
+*  iOS 13
+*  Swift 5.1
+*  Xcode 11.0 or later
+*  [Cocoapods](https://cocoapods.org)
+
+## Author
+*  Denis Fortuna 
