61f176f414c1675e74ea5504d4bae8d0c07d49ed
dfortuna <denis.fortuna@gmail.com>
2020-10-29 13:40:43 -0300

    Adding SearchController to ContactsListViewController

--- Prova_Helpie/Contacts LIst/ContactsListTableViewController.swift
+++ Prova_Helpie/Contacts LIst/ContactsListTableViewController.swift
@@ -15,19 +15,22 @@ class ContactsListTableViewController: UIViewController {
     fileprivate var loggedUserPhoto = UIImageView()
     fileprivate var loggedUserNameLabel = UILabel()
     fileprivate var tableView = UITableView()
+    fileprivate let backgroundView = UIView()
     
     fileprivate let cellIdLoggedUser = "LoggedUserCell"
     fileprivate let cellIdContact = "ContactCell"
     
-    
     fileprivate var contacts = [User]()
     fileprivate var groupedContacts = [String: [User]]()
     fileprivate var sectionTitles = [String]()
+    fileprivate var filteredContacts = [User]()
+    
+    fileprivate var isSearching = false
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
-        self.title = "Contacts"
+        self.title = "📞 Contacts"
         configureTableView()
         setAddContactButton()
         configureSearchController()
@@ -76,18 +79,55 @@ class ContactsListTableViewController: UIViewController {
     fileprivate func fetchData() {
         contacts = FakeUsers.getUsers()
         sortFormatResults()
-        tableView.reloadData()
+        formatResultData()
+    }
+    
+    fileprivate func formatResultData() {
+        //check if there is no contacts to show either if the user is searching for a contact or
+        // if there is no contact to show at all
+        if (isSearching && filteredContacts.isEmpty) || (!isSearching && groupedContacts.count == 1) {
+            formatMessageNoContacts()
+        } else {
+            backgroundView.removeFromSuperview()
+            tableView.reloadData()
+        }
     }
     
     fileprivate func sortFormatResults() {
+        //Grouping contacts by their name's Initial letter
         groupedContacts = Dictionary(grouping: contacts, by: {String($0.name.prefix(1))})
+        
+        // Adding logged user cell as first cell with no header
         guard let user = loggedUser else { return }
         groupedContacts[""] = [user]
+        
+        //Sorting headers
         sectionTitles  = groupedContacts.map{ $0.key}.sorted(by: {$0 < $1 })
-
+        
+        //Sorting names by header
+        var sortedContactGroup = [String: [User]]()
+        groupedContacts.forEach{
+            sortedContactGroup[$0.key] = $0.value.sorted(by: { $0.name < $1.name })
+        }
+        groupedContacts = sortedContactGroup
+    }
+    
+    fileprivate func formatMessageNoContacts() {
+        view.addSubview(backgroundView)
+        backgroundView.backgroundColor = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)
+        backgroundView.anchorEdges(top: tableView.topAnchor,
+                                   left: view.leftAnchor,
+                                   right: view.rightAnchor,
+                                   bottom: view.bottomAnchor,
+                                   padding: .zero)
+        
+        let messageLabel = UILabel()
+        backgroundView.addSubview(messageLabel)
+        messageLabel.anchorCenters(centerX: backgroundView.centerXAnchor,
+                                     centerY: backgroundView.centerYAnchor)
+        messageLabel.text = "No Contacts to show"
         
     }
-
 }
 
 extension ContactsListTableViewController: UITableViewDelegate {
@@ -98,32 +138,52 @@ extension ContactsListTableViewController: UITableViewDelegate {
 
 extension ContactsListTableViewController: UITableViewDataSource {
     func numberOfSections(in tableView: UITableView) -> Int {
-        sectionTitles.count
+        if isSearching {
+            return 1
+        } else {
+            return sectionTitles.count
+        }
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        let sectionTitle = sectionTitles[section]
-        guard let sectionUsers = groupedContacts[sectionTitle] else { return 0 }
-        return sectionUsers.count
+        if isSearching {
+            return filteredContacts.count
+        } else {
+            let sectionTitle = sectionTitles[section]
+            guard let sectionUsers = groupedContacts[sectionTitle] else { return 0 }
+            return sectionUsers.count
+        }
     }
     
     func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        sectionTitles[section]
+        if isSearching {
+            return ""
+        } else {
+            return sectionTitles[section]
+        }
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        switch indexPath.section {
-        case 0:
-            guard let cell = tableView.dequeueReusableCell(withIdentifier: cellIdLoggedUser) as? LoggedUserTableViewCell else { return UITableViewCell() }
-            guard let lUser = loggedUser else { return UITableViewCell() }
-            cell.formatUI(forUser: lUser)
-            return cell
-        default:
+        
+        if isSearching {
+            let user = filteredContacts[indexPath.row]
             guard let cell = tableView.dequeueReusableCell(withIdentifier: cellIdContact) as? ContactTableViewCell else { return UITableViewCell() }
-            let section = sectionTitles[indexPath.section]
-            guard let user = groupedContacts[section]?[indexPath.row] else { return UITableViewCell() }
             cell.formatUI(forContact: user)
             return cell
+        } else {
+            switch indexPath.section {
+            case 0:
+                guard let cell = tableView.dequeueReusableCell(withIdentifier: cellIdLoggedUser) as? LoggedUserTableViewCell else { return UITableViewCell() }
+                guard let lUser = loggedUser else { return UITableViewCell() }
+                cell.formatUI(forUser: lUser)
+                return cell
+            default:
+                guard let cell = tableView.dequeueReusableCell(withIdentifier: cellIdContact) as? ContactTableViewCell else { return UITableViewCell() }
+                let section = sectionTitles[indexPath.section]
+                guard let user = groupedContacts[section]?[indexPath.row] else { return UITableViewCell() }
+                cell.formatUI(forContact: user)
+                return cell
+            }
         }
     }
     
@@ -134,6 +194,18 @@ extension ContactsListTableViewController: UITableViewDataSource {
 
 extension ContactsListTableViewController: UISearchResultsUpdating {
     func updateSearchResults(for searchController: UISearchController) {
-        
+
+        let searchingText = searchController.searchBar.text!
+        if searchingText.isEmpty {
+            //when started typing or deleted everything or cancelled search
+            isSearching = false
+        } else {
+            //when typing..either with or without results to show
+            filteredContacts = contacts.filter{ $0.name.prefix(searchingText.count) == searchingText}
+                .sorted(by: { $0.name < $1.name })
+            
+            isSearching = true
+        }
+        formatResultData()
     }
 }
